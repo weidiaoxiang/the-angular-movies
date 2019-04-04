a239254df592a9b03ee3d6e2965ea797fe78e0a5
diaoxiang <diaoxiang@cxagroup.com>
2019-04-04 17:30:26 +0800

    simple toolbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import {MatToolbarModule} from '@angular/material/toolbar';
+import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatTabsModule } from '@angular/material/tabs';
+import { MatInputModule } from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -21,8 +25,12 @@ import { TopComponent } from './top/top.component';
     BrowserModule,
     BrowserAnimationsModule,
     AppRoutingModule,
+    MatIconModule,
     MatToolbarModule,
-    MatTabsModule
+    MatTabsModule,
+    MatInputModule,
+    MatButtonModule,
+    MatFormFieldModule
   ],
   providers: [],
   bootstrap: [AppComponent]
